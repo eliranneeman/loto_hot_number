8b99deed1eedfd262f0d5bdaa858f465910963c6
Eliran Neeman <53824056+eliranneeman@users.noreply.github.com>
2025-08-27 09:30:29 +0300

    Update common.js

--- common.js
+++ common.js
@@ -1,8 +1,21 @@
-// טוען פוטר משותף לכל האתר
+// טוען תפריט צד משותף לכל האתר
 fetch("/menu.html")
   .then(res => res.text())
   .then(html => {
     document.body.insertAdjacentHTML("beforeend", html);
+
+    // עכשיו שהתפריט נטען, מוסיפים את הפקודות לפתיחה וסגירה
+    const openBtn = document.getElementById("menu-open-btn");
+    const sidebar = document.getElementById("mySidebar");
+    const closeBtn = document.getElementById("menu-close-btn");
+
+    openBtn.addEventListener("click", () => {
+      sidebar.style.width = "250px"; // או רוחב מותאם
+    });
+
+    closeBtn.addEventListener("click", () => {
+      sidebar.style.width = "0";
+    });
   });
 
 // טוען באנר קוקיז משותף
