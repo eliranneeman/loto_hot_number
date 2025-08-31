b42d9142c0d31f2327d3d513dc143fa4aa9cf6b9
Eliran Neeman <53824056+eliranneeman@users.noreply.github.com>
2025-08-31 19:42:03 +0000

    Update common.js

--- common.js
+++ common.js
@@ -175,6 +175,25 @@ document.addEventListener('DOMContentLoaded', () => {
   }, 200);
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  // ניגודיות
+  if (localStorage.getItem("highContrast") === "true") {
+    document.body.classList.add("high-contrast");
+  }
+
+  // הדגשת קישורים
+  if (localStorage.getItem("highlightLinks") === "true") {
+    document.body.classList.add("highlight-links");
+  }
+
+  // גודל טקסט
+  const savedSize = localStorage.getItem("fontSize");
+  if (savedSize) {
+    document.body.style.fontSize = savedSize + "%";
+  }
+});
+
+
 /*
 // פונקציה נוספת לוודא שהחלון מוסתר גם לאחר טעינת התפריט
 setTimeout(() => {
